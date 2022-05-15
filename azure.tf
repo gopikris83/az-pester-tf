a2a9cbdba4c65e3d2b6ddf17553ee17205bc111e
Gopi krishna <gopikrishna.ravichandran@sas.se>
2022-05-15 19:41:30 +0200

    Third commit

---
+++ azure.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">=1.0"
+  backend "azurerm" {
+   subscription_id      = "05668dbd-b4a9-47bf-9be0-f93a45c7ca62"
+    resource_group_name  = "azactivedirectory-rg"
+    storage_account_name = "azadmysql2022"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.state"
+    tenant_id            = "1579c998-5e46-4c96-8a84-3ba995b0aea1"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.67.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "policy_folder" {
+  type = string
+  default = "./policies/"
+}
+
+variable "name_space" {
+  type = string
+  default = "articlePost"
+}
+variable "location" {
+  type = string
+  default = "WestEurope"
+}
